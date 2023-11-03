c2da6cce2e299b7ccc9b3e92e368ebf0e5504ad7
masatosotoma <masatosotoma@gmail.com>
2023-11-02 21:42:59 -0400

    Practiced for & while loops

---
+++ JavaScript/Week1/exercise-loops.js
@@ -0,0 +1,34 @@
+//Task1
+for (var i = 1; i <= 5; i++) {
+  console.log(i);
+}
+console.log("Counting completed!");
+
+//Task2
+for (var i = 5; i > 0; i--) {
+  console.log(i);
+}
+console.log("Countdown finished!");
+
+//Task3
+var counter = 1;
+while (counter <= 5) {
+  console.log(counter);
+  counter = counter + 1;
+}
+console.log("Counting Completed!");
+
+//Task4
+var counter2 = 5;
+while (counter2 >= 1) {
+  console.log(counter2);
+  counter2 = counter2 - 1;
+}
+console.log("Counting finished!");
+
+//Task5
+var year = 2018;
+while (year <= 2022) {
+  console.log(year);
+  year = year + 1;
+}
