cf29d1e298b1550cb9832e9a11a919b91d7e5508
masatosotoma <masatosotoma@gmail.com>
2023-11-03 22:43:22 -0400

    Exercise creating arrays

---
+++ JavaScript/Week2/exercise-arrays-&-objects.js
@@ -0,0 +1,28 @@
+//Create a new empty array literal and assign it to the variable clothes.
+var clothes = [];
+
+//Add 5 of your favorite items of clothing as strings using the push() method.
+clothes.push("shirt");
+clothes.push("socks");
+clothes.push("glove");
+clothes.push("jacket");
+clothes.push("underwear");
+console.log(clothes);
+
+//Remove the fifth piece of clothing from the array using the pop() method.
+clothes.pop();
+console.log(clothes);
+
+//Add a new piece of clothing using the push() method.
+clothes.push("sweater");
+
+//Use console.log to show the third item from the clothes array in the console.
+console.log(clothes);
+
+//Create a new empty object literal and assign it to the variable favCar.
+//Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+//Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+var favCar = {};
+(favCar.color = "black"), (favCar.covertible = true);
+console.log(favCar);
