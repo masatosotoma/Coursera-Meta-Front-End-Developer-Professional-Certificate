1bd28c2f8b639eac9c75e5028c52d320f221fa00
masatosotoma <masatosotoma@gmail.com>
2023-11-11 11:13:36 -0500

    Explained Set

---
+++ JavaScript/Week3/set.js
@@ -0,0 +1,6 @@
+
+//Use it to quickly filter an array for unique members.
+const repetitiveFruits = ["apple", "pear", "apple", "pear", "plum", "apple"];
+const uniqueFruits = new Set(repetitiveFruits);
+console.log(uniqueFruits);
+
