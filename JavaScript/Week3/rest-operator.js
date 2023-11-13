51147fc2aa0549e664ae5fff0874a42aabace8a4
masatosotoma <masatosotoma@gmail.com>
2023-11-12 23:47:52 -0500

    Explained rest operator

---
+++ JavaScript/Week3/rest-operator.js
@@ -0,0 +1,16 @@
+let top7 = [
+  "The Colosseum",
+  "The Roman Forum",
+  "The Vatican",
+  "Trevi Fountain",
+  "The Pantheon",
+  "Piazza Venezia",
+  "The Palatine Hill",
+];
+
+const [] = top7;
+
+const [first, second, third, ...secondVisit] = top7;
+
+console.log(top7);
+console.log(secondVisit);
