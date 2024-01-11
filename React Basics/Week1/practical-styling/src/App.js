fae57c24209cfd86d540e5479242ee83e4a3bdf7
masatosotoma <masatosotoma@gmail.com>
2024-01-10 23:08:35 -0500

    Apply CSS rule to components

---
+++ React Basics/Week1/practical-styling/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <Main userName="Mack" num={5} />
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
