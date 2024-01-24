446be4380b42ea30c72603005c34c6e342ecfa0b
masatosotoma <masatosotoma@gmail.com>
2024-01-23 22:41:30 -0500

    Completed create list

--- React Advanced/Week1/create-list-components/src/DessertsList.js
+++ React Advanced/Week1/create-list-components/src/DessertsList.js
@@ -1,14 +1,18 @@
-function DessertsList(props) {
-  const listItems = props.map((dessert) => {
-    const itemText = `${dessert.name} - ${dessert.calories} cal`;
-    return <li>{itemText}</li>;
-  });
-
-  return (
-    <div>
-      <ul></ul>
-    </div>
-  );
-}
-
+const DessertsList = (props) => {
+  const lowCaloriesDesserts = props.data
+    .filter((dessert) => {
+      return dessert.calories < 500;
+    })
+    .sort((a, b) => {
+      return a.calories - b.calories;
+    })
+    .map((dessert) => {
+      return (
+        <li>
+          {dessert.name} - {dessert.calories} cal
+        </li>
+      );
+    });
+  return <ul>{lowCaloriesDesserts}</ul>;
+};
 export default DessertsList;
