d4a02fc04f855e01c5962b3a266a27da00da6d9b
masatosotoma <masatosotoma@gmail.com>
2024-01-29 22:29:58 -0500

    Setting up GoalForm componenet

---
+++ React Advanced/Week2/usestate-hook/src/GoalForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function GoalForm(props) {
+  const [formData, setFormData] = React.useState({ goal: "", by: "" });
+
+  function changeHandler(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({ goal: "", by: "" });
+  }
+
+  return (
+    <>
+      <h1>My Little Lemon Goals</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="Goal"
+          value={formData.goal}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="by"
+          value={formData.by}
+          onChange={changeHandler}
+        />
+        <button>Submit Goal</button>
+      </form>
+    </>
+  );
+}
+
+export default GoalForm;
