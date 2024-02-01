4fdfdf2fccb3a83627ae2e3b9e5e0f3408c9e95a
masatosotoma <masatosotoma@gmail.com>
2024-01-31 23:03:51 -0500

    Created the goal list

--- React Advanced/Week2/usestate-hook/src/App.js
+++ React Advanced/Week2/usestate-hook/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import GoalForm from "./GoalForm";
+import ListOfGoals from "./ListOfGoals";
 
 function App() {
+  const [allGoals, updateAllGoals] = React.useState([]);
+
+  function addGoal(goal) {
+    updateAllGoals([...allGoals, goal]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals} />
     </div>
   );
 }
