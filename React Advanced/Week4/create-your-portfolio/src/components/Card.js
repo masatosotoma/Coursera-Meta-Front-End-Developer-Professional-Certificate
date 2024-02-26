5560cfb01883a1a0169011f966d4462e4deb77ab
masatosotoma <masatosotoma@gmail.com>
2024-02-25 22:06:02 -0500

    Added futured projects

--- React Advanced/Week4/create-your-portfolio/src/components/Card.js
+++ React Advanced/Week4/create-your-portfolio/src/components/Card.js
@@ -2,12 +2,36 @@ import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import ProjectsSection from "./ProjectsSection";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <div>
+      <Image src={imageSrc} alt={title} mb={4} />
+      <VStack spacing={2} align="start">
+        <Heading as="h3" size="md">
+          {title}
+        </Heading>
+        <Text>{description}</Text>
+        <HStack>
+          {/* Add a Link component for smooth scrolling */}
+          <Text
+            as="span"
+            onClick={() =>
+              handleSmoothScroll(
+                `${title.toLowerCase().replace(/\s+/g, "-")}-section`
+              )
+            }
+            cursor="pointer"
+            color="blue.500"
+          >
+            View Details
+          </Text>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </div>
+  );
 };
 
 export default Card;
