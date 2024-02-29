80539dd1f4af2391c922a2ea211437cf03e3a5e8
masatosotoma <masatosotoma@gmail.com>
2024-02-28 20:13:56 -0500

    Added Projects card sections

--- React Advanced/Week4/create-your-portfolio/src/components/Card.js
+++ React Advanced/Week4/create-your-portfolio/src/components/Card.js
@@ -2,35 +2,29 @@ import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import ProjectsSection from "./ProjectsSection";
 
 const Card = ({ title, description, imageSrc }) => {
+  // Implement the UI for the Card component according to the instructions.
+  // You should be able to implement the component with the elements imported above.
+  // Feel free to import other UI components from Chakra UI if you wish to.
   return (
-    <div>
-      <Image src={imageSrc} alt={title} mb={4} />
-      <VStack spacing={2} align="start">
-        <Heading as="h3" size="md">
-          {title}
-        </Heading>
-        <Text>{description}</Text>
-        <HStack>
-          {/* Add a Link component for smooth scrolling */}
-          <Text
-            as="span"
-            onClick={() =>
-              handleSmoothScroll(
-                `${title.toLowerCase().replace(/\s+/g, "-")}-section`
-              )
-            }
-            cursor="pointer"
-            color="blue.500"
-          >
-            View Details
+    <VStack color="black" backgroundColor="white" cursor='pointer' borderRadius='20px'>
+      <Image src={imageSrc} borderRadius="20px" />
+      <VStack spacing={4} p={4} alignItems="flex-start">
+        <VStack justifyContent="space-between" align='flex-start'>
+          <Heading as="h3" size="md">
+            {title}
+          </Heading>
+          <Text color="#64748b" fontSize="1g">
+            {description}
           </Text>
+        </VStack>
+        <HStack>
+          <p>See More</p>
           <FontAwesomeIcon icon={faArrowRight} size="1x" />
         </HStack>
       </VStack>
-    </div>
+    </VStack>
   );
 };
 
