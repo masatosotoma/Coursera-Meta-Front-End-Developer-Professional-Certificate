54c7d4253cfa32edb5700e9fd58224f95d9d3e7d
masatosotoma <masatosotoma@gmail.com>
2024-03-26 23:04:47 -0400

    Added meta tags

--- Front-End Developer Capstone/little-lemon-project/src/App.js
+++ Front-End Developer Capstone/little-lemon-project/src/App.js
@@ -4,10 +4,15 @@ function App() {
   return (
     <>
       <img></img>
-      <Header/>
-      <Nav/>
-      <Main/>
-      <Footer/>
+      <Header>
+        <meta name="description" content="your text goes here" />
+        <meta name="og:title" content="" />
+        <meta name="og:description" content="" />
+        <meta name="og:image" content="" />
+      </Header>
+      <Nav />
+      <Main />
+      <Footer />
     </>
   );
 }
