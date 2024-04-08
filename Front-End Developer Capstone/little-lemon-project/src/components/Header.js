9ae844ea3ee769af37ce395ad9dc3870e146fd20
masatosotoma <masatosotoma@gmail.com>
2024-04-07 22:00:01 -0400

    Started building Header component

---
+++ Front-End Developer Capstone/little-lemon-project/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <section>
+                {/*banner texts*/}
+                <div>
+                    <h2>Little Lemon</h2>
+                    <h3>Chicago</h3>
+                    <p>We are a family owned Mediterraneran restaurant, focused on traditional receipes served with a modern twist</p>
+                </div>
+            </section>
+</header>
+    )
+}
+
+export default Header;
