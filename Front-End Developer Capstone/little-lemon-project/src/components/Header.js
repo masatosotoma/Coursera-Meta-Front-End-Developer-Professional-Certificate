cc1e53ae02e342159335eee0db597e8de0e2947e
masatosotoma <masatosotoma@gmail.com>
2024-04-14 20:27:13 -0400

    Adding header button + img

--- Front-End Developer Capstone/little-lemon-project/src/components/Header.js
+++ Front-End Developer Capstone/little-lemon-project/src/components/Header.js
@@ -1,18 +1,27 @@
 import React from "react";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <section>
-                {/*banner texts*/}
-                <div>
-                    <h2>Little Lemon</h2>
-                    <h3>Chicago</h3>
-                    <p>We are a family owned Mediterraneran restaurant, focused on traditional receipes served with a modern twist</p>
-                </div>
-            </section>
-</header>
-    )
-}
+  return (
+    <header className="header">
+      <section>
+        {/*banner texts*/}
+        <div>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are a family owned Mediterraneran restaurant, focused on
+            traditional receipes served with a modern twist
+          </p>
+          <button aria-label="On Click">Reserve Table</button>
+        </div>
+
+        {/*banner image*/}
+        <div>
+          <img src="" alt="" />
+        </div>
+      </section>
+    </header>
+  );
+};
 
 export default Header;
