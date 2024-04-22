76047926fc828e3043c5cdeda461cca4a85ada74
masatosotoma <masatosotoma@gmail.com>
2024-04-21 20:56:33 -0400

    Booking component added attributes

--- Front-End Developer Capstone/little-lemon-project/src/components/Booking.js
+++ Front-End Developer Capstone/little-lemon-project/src/components/Booking.js
@@ -1,7 +1,14 @@
 import React from "react";
+import BookingForm from "./BookingForm";
 
 const Booking = (props) => {
-  return <div></div>;
+  return (
+    <BookingForm
+      availableTimes={props.availableTimes}
+      dispatch={props.dispatch}
+      submitForm={props.submitForm}
+    />
+  );
 };
 
 export default Booking;
