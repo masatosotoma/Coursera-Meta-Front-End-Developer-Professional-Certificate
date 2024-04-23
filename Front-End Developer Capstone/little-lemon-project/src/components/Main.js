093bda25c7ebdee37e88a3f179d7c523a4e2f4fd
masatosotoma <masatosotoma@gmail.com>
2024-04-22 22:36:41 -0400

    setting up Main component functions

--- Front-End Developer Capstone/little-lemon-project/src/components/Main.js
+++ Front-End Developer Capstone/little-lemon-project/src/components/Main.js
@@ -1,13 +1,45 @@
-import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import React, { useReducer } from "react";
+import { Route, Routes, useAsyncValue, useNavigate } from "react-router-dom";
+import Header from "./Header";
+import Booking from "./Booking";
 
 const Main = () => {
-    return (
-        <main>
-            <Routes>
-                <Route path='/' element={<Header/>}/>
 
-            </Routes>
-        </main>
-    )
-}
+    
+
+  const initialState = { availableTimes: fetchAPI(new Date()) };
+  const [state, dispatch] = useReducer(updateTimes, initialState);
+
+  function updateTimes(state, date) {
+    return {
+      availableTimes: fetchAPI(new Date()),
+    };
+  }
+
+  const navigate = useNavigate();
+  function submitForm(formData) {
+    if (submitAPI(formData)) {
+      navigate("/confirmed");
+    }
+  }
+
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route
+          path="/booking"
+          element={
+            <Booking
+              availableTimes={state}
+              dispatch={dispatch}
+              submitForm={submitForm}
+            />
+          }
+        />
+      </Routes>
+    </main>
+  );
+};
+
+export default Main;
