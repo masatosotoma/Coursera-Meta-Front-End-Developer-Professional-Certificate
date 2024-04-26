f2aae834d5dfcbfa763d3cf77a62d66d755d51da
masatosotoma <masatosotoma@gmail.com>
2024-04-25 23:27:43 -0400

    Started createing Menu component

--- Front-End Developer Capstone/little-lemon-project/src/App.js
+++ Front-End Developer Capstone/little-lemon-project/src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import BookingForm from "./components/BookingForm";
+import Main from "./components/Main";
 
 function App() {
   return (
     <>
       <Nav />
-      <Header />
-      <BookingForm />
+      <Main />
     </>
   );
 }
