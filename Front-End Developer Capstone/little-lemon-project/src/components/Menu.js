c5f53817fe94d59432bc18df32f666860c7f6e03
masatosotoma <masatosotoma@gmail.com>
2024-04-27 22:57:30 -0400

    Creating menu

--- Front-End Developer Capstone/little-lemon-project/src/components/Menu.js
+++ Front-End Developer Capstone/little-lemon-project/src/components/Menu.js
@@ -1,7 +1,21 @@
 import React from "react";
+import recipes from "../recipes";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu-container">
+      <div>
+        <h2>This weeks specials</h2>
+        <button>Order Menu</button>
+      </div>
+
+      <div>
+        {recipes.map((receipe) => {
+          recipe.id;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
---
+++ Front-End Developer Capstone/little-lemon-project/src/recipes.js
@@ -0,0 +1,10 @@
+const receipes = [
+  {
+    id: 1,
+    title: "Greek salad",
+    price: 12.99,
+    image: A,
+    description:
+      "the famous greek salad of crispy lettuce, peppers, olives and our Chicago style fota cheese, garnished with crunchy garlic and rosemary croutons",
+  },
+];
