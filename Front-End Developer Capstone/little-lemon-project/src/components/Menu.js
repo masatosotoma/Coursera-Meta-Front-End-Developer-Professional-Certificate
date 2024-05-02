12c08d2e04f5dfb6bce29a65e2835c477f31daef
masatosotoma <masatosotoma@gmail.com>
2024-05-01 22:03:36 -0400

    Corrected typo mistakes for Menu components

--- Front-End Developer Capstone/little-lemon-project/src/components/Menu.js
+++ Front-End Developer Capstone/little-lemon-project/src/components/Menu.js
@@ -1,26 +1,26 @@
 import React from "react";
-import recipes from "../recipes.js"
+import recipes from "../recipes";
 
 const Menu = () => {
   return (
     <div className="menu-container">
-      <div>
-        <h2>This weeks specials</h2>
+      <div className="menu-header">
+        <h2>This Weeks specials!</h2>
         <button>Order Menu</button>
       </div>
 
-      <div className="cards">
-        {recipes.map((recipe) => <div key={recipe.id} className='menu-items'>
-          <img src={recipe.image} alt='' />
-          <div>
+      <div>
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="menu-items">
+            <img src={recipe.image} alt="" />
             <div>
               <h5>{recipe.title}</h5>
               <p>{recipe.price}</p>
             </div>
             <p>{recipe.description}</p>
             <button className="orderBtn">Order Now</button>
-          </div>)
-        };
+          </div>
+        ))}
       </div>
     </div>
   );
