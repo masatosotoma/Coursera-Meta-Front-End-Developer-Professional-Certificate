9d18c4451abae6116371185b2b6c0b69d0ebe8b6
masatosotoma <masatosotoma@gmail.com>
2024-05-03 23:04:17 -0400

    added sweet alert for handle order

--- Front-End Developer Capstone/little-lemon-project/src/components/Menu.js
+++ Front-End Developer Capstone/little-lemon-project/src/components/Menu.js
@@ -1,7 +1,28 @@
 import React from "react";
 import recipes from "../recipes";
+import Swal from "sweetalert2";
 
 const Menu = () => {
+  const handleOrder = (id) => {
+    console.log(id, "id is clicked");
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
   return (
     <div className="menu-container">
       <div className="menu-header">
@@ -18,7 +39,9 @@ const Menu = () => {
               <p>{recipe.price}</p>
             </div>
             <p>{recipe.description}</p>
-            <button className="orderBtn">Order Now</button>
+            <button className="orderBtn" onClick={() => handleOrder(recipe.id)}>
+              Order Now
+            </button>
           </div>
         ))}
       </div>
