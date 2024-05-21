d0c206c4da5259adf3db621ff7c547fd7723f9eb
masatosotoma <masatosotoma@gmail.com>
2024-05-20 22:33:32 -0400

    Setup the App.js with Router

--- Front-End Developer Capstone/little-lemon-project-2/src/App.js
+++ Front-End Developer Capstone/little-lemon-project-2/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import BookingPage from "./pages/BookingPage";
+import ConfirmedBooking from "./pages/ConfirmedBooking";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" exact elememt={<HomePage />}></Route>
+        <Route path="/booking" elememt={<BookingPage />}></Route>
+        <Route path="/confirmed" elememt={<ConfirmedBooking />}></Route>
+      </Routes>
+    </>
   );
 }
 
